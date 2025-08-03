23e39014711fe1a98ca69f80c27daef616cac2aa
KORWOT <coli4054@gmail.com>
2025-08-03 21:34:35 +0900

    프로젝트 롤백

--- Assets/Scripts/Core/Interfaces/IRewardManager.cs
+++
@@ -1,9 +0,0 @@
-using DungeonMaster.Data;
-
-namespace DungeonMaster.Core.Interfaces
-{
-    public interface IRewardManager
-    {
-        void DistributeRewards(int totalScore, PlayerData playerData);
-    }
-}
